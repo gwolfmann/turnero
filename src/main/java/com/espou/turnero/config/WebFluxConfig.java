1264dd5bb07d1356cb91ec6acd5b513d6db881cd
gustavowolfmann <gwolfmann@gmail.com>
2023-07-20 12:11:43 -0300

    allow localhost angular app connections

---
+++ src/main/java/com/espou/turnero/config/WebFluxConfig.java
@@ -0,0 +1,19 @@
+package com.espou.turnero.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .maxAge(3600);
+    }
+}
